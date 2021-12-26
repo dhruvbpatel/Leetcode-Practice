058c30b7cc4cd4b433d2f4083e286213bc413f09
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2021-12-26 17:01:57 -0500

    Time: 16 ms (70.51%), Space: 12.4 MB (38.42%) - LeetHub

---
+++ target-sum/target-sum.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    
+    int countSubsetSum(vector<int>& nums,int n,int sum){
+        
+        
+        vector<vector<int>> dp(n+1,vector<int>(sum+1));
+        
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0) dp[i][j]=false;
+                if(j==0) dp[i][j]=true;
+            }
+        }
+        
+        dp[0][0]=1;
+        
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                
+                
+                if(nums[i-1]<=j){
+                    
+                    dp[i][j] = dp[i-1][j]+dp[i-1][j-nums[i-1]];
+                    
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+                
+            }
+        }
+        
+        return dp[n][sum];
+        
+        
+    }
+    
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        target = abs(target);
+        
+        int n = nums.size();
+        
+        int sum = 0;
+        
+        for(auto i:nums) sum+=i;
+        
+        int s1 = (target+sum)/2;
+        
+        if(sum<target || (sum+target)%2!=0)
+            return 0;
+        
+        return countSubsetSum(nums,n,s1);
+        
+        
+        
+        
+        
+    }
+};
