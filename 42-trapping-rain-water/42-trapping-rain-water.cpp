d68e89b41ad4463aad0e50a603414f949aea7502
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-01-11 18:27:19 -0500

    Time: 3 ms (98.66%), Space: 15.6 MB (84.57%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,35 +2,79 @@ class Solution {
 public:
     int trap(vector<int>& arr) {
         
-        stack<int> st;
+        // 2 pointer approach;
         
-        int n = arr.size();
+        int leftmax = 0;
+        int rightmax = 0;
+        
+        int l = 0;
+        int r = arr.size()-1;
         
         int ans = 0;
         
-        for(int i=0;i<n;i++){
+        
+        while(l<=r){
             
-            while(!st.empty() and arr[st.top()]<arr[i]){  // if we found curr element is greatert than top of stack do while it is not
+            if(arr[l]<=arr[r]){
                 
-                int curr = st.top(); // top index
-                st.pop();
+                if(arr[l]>=leftmax) leftmax = arr[l];
                 
-                if(st.empty()){
-                    break; // this handles if there is not left boundary i.e bigger element is at 1th index
+                else{
+                    ans+= leftmax - arr[l];
                 }
                 
-                int diff = i-st.top()-1; // length difference 
-                // subtract curr because current element height will be not storing water 
-                ans+= (min(arr[st.top()],arr[i])-arr[curr])*diff;
-                            
+                l++;
                 
+            }else{
+                // when arr[r]<arr[l];
+                
+                if(arr[r]>=rightmax){
+                    rightmax = arr[r];
+                }else{
+                    ans+= rightmax-arr[r];
+                }
+                
+                r--;
             }
-            
-            // if not greater than top 
-            st.push(i);
             
         }
         
         return ans;
+        
+        
+        
+        
+        
+        // stack approach;
+//         stack<int> st;
+        
+//         int n = arr.size();
+        
+//         int ans = 0;
+        
+//         for(int i=0;i<n;i++){
+            
+//             while(!st.empty() and arr[st.top()]<arr[i]){  // if we found curr element is greatert than top of stack do while it is not
+                
+//                 int curr = st.top(); // top index
+//                 st.pop();
+                
+//                 if(st.empty()){
+//                     break; // this handles if there is not left boundary i.e bigger element is at 1th index
+//                 }
+                
+//                 int diff = i-st.top()-1; // length difference 
+//                 // subtract curr because current element height will be not storing water 
+//                 ans+= (min(arr[st.top()],arr[i])-arr[curr])*diff;
+                            
+                
+//             }
+            
+//             // if not greater than top 
+//             st.push(i);
+            
+//         }
+        
+//         return ans;
     }
 };
