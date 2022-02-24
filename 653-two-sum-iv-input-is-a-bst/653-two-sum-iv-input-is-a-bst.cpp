f7e20143a342b06894432386149a98b4b18698a5
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-02-23 21:25:24 -0600

    Time: 52 ms (63.10%), Space: 36.8 MB (81.81%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,36 +9,68 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    void inorder(vector<int>& io,TreeNode* root){
-        if(root==NULL){
-            return;
-        }
-        
-        inorder(io,root->left);
-        io.push_back(root->val);
-        inorder(io,root->right);
+class BSTIterator{
+    stack<TreeNode*> st;
+    bool reverse = true;
+    // reverse -> true -> before -> i.e opposite traversal of next; i.e end from inorder
+    //reverse->false->next traversal i.e from start in inorder
+    
+    public:
+    
+    // BSTIterator(TreeNode* root,bool i)
+    
+    BSTIterator(TreeNode* root,bool isReverse){
+        reverse = isReverse;
+        pushAll(root);
+    }
+     /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !st.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *tmpNode = st.top();
+        st.pop();
+        if(!reverse) pushAll(tmpNode->right);
+        else pushAll(tmpNode->left);
+        return tmpNode->val;
     }
     
-    bool findTarget(TreeNode* root, int k) {
-        vector<int> io;
-        
-        inorder(io,root);
-        
-        int l= 0,r=io.size()-1;
-        
-        while(l<r){
-            if(io[l]+io[r]==k){
-                return true;
-            }else if(io[l]+io[r]>k){
-                r--;
-            }else{
-                l++;
-            }
+    void pushAll(TreeNode *node) {
+        for(;node != NULL; ) {
+             st.push(node);
+             if(reverse == true) {
+                 node = node->right; 
+             } else {
+                 node = node->left; 
+             }
         }
+    }
+
+    
+    
+};
+
+class Solution {
+public:
+//     
+    
+    bool findTarget(TreeNode* root, int k) {
+        // naive approach is to get inorder in array and search for 2 numbers;
+        // optimized approach is to use modified version of BST Iterator';
         
-        return false;
+        if(!root) return false; 
+        BSTIterator l(root, false); 
+        BSTIterator r(root, true); 
         
+        int i = l.next(); 
+        int j = r.next(); 
+        while(i<j) {
+            if(i + j == k) return true; 
+            else if(i + j < k) i = l.next(); 
+            else j = r.next(); 
+        }
+        return false; 
     }
 };
