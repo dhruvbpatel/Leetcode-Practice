4a9db15aa250dc2132035df932b5505752dc3b6c
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-02-24 22:32:40 -0600

    Time: 52 ms (46.30%), Space: 57.9 MB (48.58%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -1,56 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * }; 
- */
+
 class Solution {
-    private:
-        TreeNode* first;
-        TreeNode* middle;
-        TreeNode* last;
-        TreeNode* prev;
-    
-public:
-    
-    void inorder(TreeNode* root){
-        if(root==NULL) return;
+private: 
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last; 
+private: 
+    void inorder(TreeNode* root) {
+        if(root == NULL) return; 
         
         inorder(root->left);
         
-        if(prev!=NULL and (root->val<prev->val)){
-            
-            if(first == NULL){
+        if (prev != NULL && (root->val < prev->val))
+        {
+           
+            // If this is first violation, mark these two nodes as
+            // 'first' and 'middle'
+            if ( first == NULL )
+            {
                 first = prev;
                 middle = root;
-            }else{
-                last = root;
             }
-            
+ 
+            // If this is second violation, mark this node as last
+            else
+                last = root;
         }
-        
+ 
+        // Mark this node as previous
         prev = root;
-        inorder(root->right);
-        
+        inorder(root->right); 
     }
-    
-    
+public:
     void recoverTree(TreeNode* root) {
-        
-        first = middle = last = NULL;
-        prev = new TreeNode(INT_MIN);
-        if(root==NULL) return;
-        
+        first = middle = last = NULL; 
+        prev = new TreeNode(INT_MIN); 
         inorder(root);
-        
-        if(first and last) swap(first->val , last->val);
-        else if(first and middle) swap(first->val,middle->val);
-        
-        
+        if(first && last) swap(first->val, last->val); 
+        else if(first && middle) swap(first->val, middle->val); 
     }
 };
