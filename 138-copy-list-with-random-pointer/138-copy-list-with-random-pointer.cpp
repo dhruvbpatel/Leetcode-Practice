26539cb24e7cf0aa706b36c33f1f2afac106ca0e
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-04-02 23:25:52 -0500

    Time: 17 ms (24.91%), Space: 11.3 MB (63.09%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,99 +17,62 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+       
         
         Node* iter = head;
         Node* front = head;
         
-        while(iter){
+        // step 1: create copies and attach
+        
+        
+        while(iter!=NULL){
             front = iter->next;
+            
             Node* n = new Node(iter->val);
+            
             iter->next = n;
             n->next = front;
             iter = front;
         }
         
+        
+        // step 2: assign random pointer to copy
+        
+        
         iter = head;
         
-        while(iter){
+        while(iter!=NULL){
+            
             if(iter->random!=NULL){
-                iter->next->random = iter->random->next;  
+                iter->next->random = iter->random->next;
+                
             }
+            
             iter = iter->next->next;
         }
         
+        
+        // step 3: detach ll
+        
+        iter = head;
         Node* dummy = new Node(0);
         Node* temp = dummy;
         
-        iter = head;
-        
-        while(iter){
+        while(iter!=NULL){
+            
             front = iter->next->next;
+            
             temp->next = iter->next;
+            
             iter->next = front;
-            temp = temp->next;
+            
+            temp= temp->next;
+            
             iter = front;
+            
         }
         
         return dummy->next;
         
-    } 
-        
-        
-//         Node* iter = head;
-//         Node* front = head;
-        
-//         // step 1: create copies and attach
-        
-        
-//         while(iter!=NULL){
-//             front = iter->next;
-            
-//             Node* n = new Node(iter->val);
-            
-//             iter->next = n;
-//             n->next = front;
-//             iter = front;
-//         }
-        
-        
-//         // step 2: assign random pointer to copy
-        
-        
-//         iter = head;
-        
-//         while(iter!=NULL){
-            
-//             if(iter->random!=NULL){
-//                 iter->next->random = iter->random->next;
-                
-//             }
-            
-//             iter = iter->next->next;
-//         }
-        
-        
-//         // step 3: detach ll
-        
-//         iter = head;
-//         Node* dummy = new Node(0);
-//         Node* temp = dummy;
-        
-//         while(iter!=NULL){
-            
-//             front = iter->next->next;
-            
-//             temp->next = iter->next;
-            
-//             iter->next = front;
-            
-//             temp= temp->next;
-            
-//             iter = front;
-            
-//         }
-        
-//         return dummy->next;
-        
-//     }
+    }
 };
