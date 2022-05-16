48bc48ba7c51439eea30566ce11d33330f79e501
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-05-16 00:50:42 -0500

    Time: 70 ms (5.33%), Space: 13.9 MB (21.33%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,38 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        ans = ""
+        
+        for i in range(len(s)):
+            ## if not closing bracket add in stack
+            if s[i]!="]":                  
+                stack.append(s[i])
+                
+            #if closing bracket proces
+            else:                          
+                substr = ""
+                 ## while we find opening bracket
+                while stack[-1]!="[":
+                    substr = stack.pop()+substr
+                stack.pop() ## pop opening bracket
+                
+                val = ""
+                
+                # get number to be multiplied , can be 2 digits
+                while stack and stack[-1].isdigit():
+                    val = stack.pop()+val 
+                
+                stack.append(int(val)*substr)
+                
+        return "".join(stack)
+                
+                
+                    
+                    
+                    
+                    
+                    
+                
+                
+                
+        
