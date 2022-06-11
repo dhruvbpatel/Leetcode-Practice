248f010eb56a3a75d8306c7862ea3e12a54a1869
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-06-11 13:04:00 -0500

    Time: 324 ms (70.77%), Space: 27.7 MB (61.75%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -9,18 +9,19 @@ class Solution:
     
     def helper(self,root,p,q):
         
-        if root==None:
+        if root==None: #if root is none return
             return root
         
+        ##iterate over all nodes
         lans = self.helper(root.left,p,q)
         rans = self.helper(root.right,p,q)
         
-        
+        #if root is p or root is q ,update count of nodes and return node
         if root == p or root==q:
             self.count_node+=1
-            return root
+            return root  ## here we return because if 2nd node is descendant of current node then curr is LCA
             
-
+        
         if lans is None:
             return rans
         elif rans is None:
@@ -31,14 +32,14 @@ class Solution:
     
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        self.count_node = 0
+        self.count_node = 0  ## to check if both nodes are found
         
         ans = self.helper(root,p,q)
         
-        if(self.count_node==2):
+        if(self.count_node==2): #if only both are found return ans
             return ans
         else:
-            return None
+            return None # else none
         
         
         
