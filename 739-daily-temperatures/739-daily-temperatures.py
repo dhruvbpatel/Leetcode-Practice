9d9994a99eeee17e92d7c985cf09674906f7b600
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-06-26 11:30:14 -0400

    Time: 2349 ms (5.70%), Space: 25.5 MB (18.04%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,22 +1,28 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
+        #make ans array with zero
         ans = [0]*len(temperatures)
-        st = []
+        st = [] #maintain stack: monotonic decreasing stack
         
         for i in range(len(temperatures)):
             
-            curr = temperatures[i]
+            curr = temperatures[i] #for eah elements
             
+            # if curr is bigger than stack top, do until it is smalller or stack empty
             while(len(st)!=0 and temperatures[st[-1]]<curr):
                 
+                # get prev idx
                 prev = st[-1]
-                st.pop()
-                diff = i - prev
-                ans[prev] = diff
+                st.pop() #pop
+                diff = i - prev  #get diff 
+                # now as our stack is storing decreasing temperatues, when we get first bigger idx, then 
+                # for that idx diff id the no of days when we get new max temperatures
+                ans[prev] = diff 
             
+            # in any other case append idx in stack when elements are smaller
             st.append(i)
         
+        
         return ans
-            
         
