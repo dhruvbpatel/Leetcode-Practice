d1a0f2dd291b6aece699f3e7760ad7eb2e837537
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-07-20 22:22:56 -0400

    Time: 35 ms (87.22%), Space: 14.4 MB (8.00%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -5,26 +5,31 @@
 #         self.next = next
 class Solution:
     
-    nxt = None
+    
     
     def reverseFirstN(self,head,n):
-        self.nxt = None
-        if n==1:
-            self.nxt = head.next
+        self.nxt = None #nxt node
+        
+        if n==1: #if only one node is to be reverse then mark nxt and return
+            self.nxt = head.next ##mark next , useful when backtracked
             return head
         
-        last = self.reverseFirstN(head.next,n-1)
-        head.next.next=head
-        head.next = self.nxt
-        return last
+        last = self.reverseFirstN(head.next,n-1) # recursively reverse , returns lastnode
+        head.next.next=head #original still connected
+        head.next = self.nxt #now connect first node to n+1 node,i.e nxt
+        return last # return last which is new head
         
     
     def reverseBetween(self, head: Optional[ListNode], m: int, n: int) -> Optional[ListNode]:
         
-        if m==1:
-            return self.reverseFirstN(head,n)
+        if head is None or m==n:
+            return head
         
-        head.next = self.reverseBetween(head.next,m-1,n-1)
+        if m==1:
+            return self.reverseFirstN(head,n) ## if first pointer is 1st node,
+        # this boils down to reverse first N nodes only
+        
+        head.next = self.reverseBetween(head.next,m-1,n-1) #if m!=1 then reduce them
         
         return head
         
