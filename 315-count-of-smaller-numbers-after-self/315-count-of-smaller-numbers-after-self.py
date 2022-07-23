526faf5af6501884027805d71ffefb57aa8bf6a8
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-07-22 22:04:34 -0400

    Time: 2208 ms (95.43%), Space: 33.7 MB (64.40%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -3,16 +3,38 @@ from sortedcontainers import SortedList
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         
-        s = SortedList()
+        
+        
+        s = SortedList() # sorted list
         output = []
+        
         
         
         for n in nums[::-1]:
             
-            ans = SortedList.bisect_left(s,n)
-            output.append(ans)
-            s.add(n)
+            #bisect left find position index where n can be inserted in sorted position 
+            ans = SortedList.bisect_left(s,n) 
+            output.append(ans) #append idx in output
+            s.add(n) #also add n in sorted list
+            
+#             print(s)
+#             print(ans)
         
-        return output[::-1]
+        return output[::-1] # ans is in reverse manner so reverse it
+    
+    
+"""
+ex: n=6 #started from ending
+at this stage s =[1]
+with bisect we find idx as 1,
+there append it in output
+
+same way for other numbers, reverse output at end
+
+"""
+    
+    # TC: nlogn for sorted list
+    # sorted list insertion time is logn
+    
             
         
