99783690e7e59d9a258f4540a7d7487bef5d2339
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-07-25 22:37:07 -0400

    Time: 1421 ms (46.57%), Space: 18.2 MB (22.85%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
         nums.sort()
         ans = []
         
@@ -8,39 +7,40 @@ class Solution:
             
             if(i==0 or (i>0 and nums[i]!=nums[i-1])):
                 
+                target = 0 - nums[i]
+                
                 low = i+1
                 high = len(nums)-1
-                temp = 0-nums[i]
                 
                 while(low<high):
                     
-                    if(nums[low]+nums[high]==temp):
-                        
-                        t = []
-                        t.append(nums[i])
-                        t.append(nums[low])
-                        t.append(nums[high])
-                        ans.append(t)
+                    if(nums[low]+nums[high]==target):
+                        temp = []
+                        temp.append(nums[i])
+                        temp.append(nums[low])
+                        temp.append(nums[high])
+                        ans.append(temp)
                         
                         while(low<high and nums[low]==nums[low+1]):
                             low+=1
                         
                         while(low<high and nums[high]==nums[high-1]):
                             high-=1
-                            
+                        
                         low+=1
                         high-=1
-                        
-                        
-                    elif(nums[low]+nums[high]<temp):
+                    
+                    elif(nums[low]+nums[high]<target):
                         low+=1
                     else:
                         high-=1
+        
         return ans
-                    
                         
+                            
+                    
+                    
                 
-                
-            
-            
+        
+        
         
