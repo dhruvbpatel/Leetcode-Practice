dc0abe7c8f89912315b1a3256ad06bd78f5c97a3
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-07-30 14:23:31 -0400

    Time: 2412 ms (5.01%), Space: 18.7 MB (38.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,7 +1,16 @@
 class Solution:
     def wordSubsets(self, A, B):
         cnt = Counter()
+        
         for b in B:
-            cnt |= Counter(b)
-            
-        return [a for a in A if not cnt - Counter(a)]
+            cnt |= Counter(b) #update cnt with max max freq value
+        
+        #after this cnt will have max freq with keys that form biggest subset
+        # if this biggest subset is in word of A then that word is superset of b
+        # to check superset, simply cnt-Counter(a), because cnt should be subset of A 
+        # to satisfy conditon, if everything in cnt is in Counter(a) then 
+        # cnt-counter(a) returns empty set even though counter(a) has some other chars
+        # thus counter(a) is universal , so add to set
+        
+        
+        return [a for a in A if not cnt - Counter(a)]  
