5ccdef7df989ab67d4144f1305aaf282f93c5bb2
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-07-30 15:40:23 -0400

    Time: 494 ms (16.87%), Space: 25.9 MB (71.14%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        
+        ans = sys.maxsize
+        l = 0
+        total = 0
+        
+        for r in range(len(nums)):
+            total+=nums[r] #subarray l-r sum
+            
+            while(total>=target): #current subarray sum is greater or equal try for smaller
+                ans = min(ans,r-l+1) #current length and update if min
+                total-=nums[l] #remove left sum
+                l+=1 # increment left pointer
+            
+        return ans if ans is not sys.maxsize else 0
+        
+                
+        
