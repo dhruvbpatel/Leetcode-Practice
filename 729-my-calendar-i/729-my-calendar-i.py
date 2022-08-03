281b876a9e4c2d761c13f7840083e5ed4acc52a0
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-08-02 22:20:35 -0400

    Time: 313 ms (79.15%), Space: 14.7 MB (58.88%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,22 +1,45 @@
+class Node:
+    
+    def __init__(self,start,end):
+        self.start = start
+        self.end = end
+        self.left = None
+        self.right = None
+
 class MyCalendar:
 
     def __init__(self):
-        self.arr = []
+        self.root = None
+    
+    def buildTree(self,start,end,node):
         
-
+        if start>=node.end:
+            if node.right:
+                return self.buildTree(start,end,node.right)
+            
+            else:
+                node.right = Node(start,end)
+                return True
+        
+        elif end<=node.start:
+            
+            if node.left:
+                return self.buildTree(start,end,node.left)
+            else:
+                node.left = Node(start,end)
+                return True
+        else:
+            
+            return False
+            
+        
     def book(self, start: int, end: int) -> bool:
         
-        for s,e in self.arr:
-            if s<end and start<e:
-                return False
+        if not self.root:
+            self.root = Node(start,end)
+            return True
         
-        self.arr.append((start,end))
-        
-        return True
-        
-        
-        
-
+        return self.buildTree(start,end,self.root)
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
