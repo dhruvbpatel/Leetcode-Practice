354abc1a9e9646b9b4c44d2e972cbcdbddf3c9d7
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-08-13 10:24:32 -0400

    Time: 281 ms (98.48%), Space: 16.5 MB (64.75%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,33 +1,32 @@
 class Solution:
     
-    def mark_curr(self,grid,i,j,nrow,ncol):
+    def mark(self,grid,r,c,n,m):
         
-
-        if(i<0 or i>=nrow or j<0 or j>=ncol or grid[i][j]!='1'):
+        if r<0 or r>=n or c<0 or c>=m or grid[r][c]!='1':
             return
+                
+        grid[r][c]='2'
         
-        grid[i][j]="2"
-        
-        self.mark_curr(grid,i+1,j,nrow,ncol)
-        self.mark_curr(grid,i,j+1,nrow,ncol)
-        self.mark_curr(grid,i-1,j,nrow,ncol)
-        self.mark_curr(grid,i,j-1,nrow,ncol)
+        self.mark(grid,r+1,c,n,m)
+        self.mark(grid,r,c+1,n,m)
+        self.mark(grid,r,c-1,n,m)
+        self.mark(grid,r-1,c,n,m)
         
         return
-        
     
     def numIslands(self, grid: List[List[str]]) -> int:
-        nrow = len(grid)
-        ncol = len(grid[0])
+        n = len(grid)
+        m = len(grid[0])
         
-        num_island = 0
-
-        for i in range(nrow):
-            for j in range(ncol):
-                if(grid[i][j]=="1"):
-                    self.mark_curr(grid,i,j,nrow,ncol)
-                    num_island+=1
+        ans = 0
         
-        return num_island
+        for r in range(n):
+            for c in range(m):
+                
+                if grid[r][c]=='1':
+                    self.mark(grid,r,c,n,m)
+                    ans+=1
+        
+        return ans
         
         
