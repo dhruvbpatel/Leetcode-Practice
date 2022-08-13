dfd1a9c339e02aca39c4b7d3b4d6b134b7a3cc81
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-08-13 18:55:58 -0400

    Time: 116 ms (94.04%), Space: 15 MB (97.53%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,53 +1,34 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        
-#         pd = a:1
-#             b:1
-#             c:1
-        
-#         sd = c:1
-#             b:1
-#             a:1
-                
-#         if len(p)>len(s):return [];
-        
-#         start = 0
-#         end = len(p)-1
-        
-#         while end!=len(s)
-#         if pd==sd ->append(idx)
-        
-#         sd[s[start]]-=1
-#         if sd[s[start]]==0:
-#             del sd[s[start]]
-#         start++
-#         sd[s[end]]
-#         end++
-#         if end>len(s):
-#             break
 
+        #edge case
         if len(s)<len(p):
             return []
         
         pd = defaultdict(int)
         
+        # dict for p
         for c in p:
             pd[c]+=1
         
+        # dict for s
         sd = defaultdict(int)
         
+        # define window pointers
         end = 0
         start = 0
         
+        # for p size window in s , add window counter
         for i in range(len(p)):
             sd[s[i]]+=1
             end+=1
-        end-=1
+            
+        end-=1 #adjust end pointer
+        
         ans = []
         
         # print(sd)
         
-        i = 0
         
         for i in range(len(s)-len(p)+1):
             
@@ -69,8 +50,6 @@ class Solution:
             
             sd[s[end]]+=1
             
-            
-            i+=1
             
         return ans
                 
