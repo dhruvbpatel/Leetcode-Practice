cc321add91a245d636f43e8dd77c7e93b1423b7a
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-08-13 12:45:50 -0400

    Time: 372 ms (26.31%), Space: 14.1 MB (96.18%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,26 +1,24 @@
-from collections import deque
 class Solution:
+    
     def dfs(self,isConnected,visited,curr):
+        
         for j in range(len(isConnected)):
-            if(isConnected[curr][j]==1 and visited[j]==0):
+            
+            if isConnected[curr][j]==1 and visited[j]==0:
                 visited[j]=1
                 self.dfs(isConnected,visited,j)
-            
-            
+        return
+        
+    
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
         visited = [0]*len(isConnected)
-        ans = 0
+        ans=0
+        
         for i in range(len(isConnected)):
+            
             if visited[i]==0:
                 self.dfs(isConnected,visited,i)
                 ans+=1
         
         return ans
-        
-                
-
-                
-                
-            
-                
-        
