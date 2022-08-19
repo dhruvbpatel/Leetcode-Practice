33d9f03ed7f27f8d98a82a636aef24fd95b81434
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-08-18 22:36:25 -0400

    Time: 213 ms (58.53%), Space: 14.7 MB (64.07%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,62 +1,29 @@
 class Solution:
-    
-    def find(self,matrix,n,row,col,ans,temp):
-        
-        if row>=n:
-            self.ans = min(temp,ans)
-            return
-        
-        temp+=matrix[row][col]
-        
-        if col-1>=0:
-            self.find(matrix,n,row+1,col-1,self.ans,temp)
-        
-        self.find(matrix,n,row+1,col,self.ans,temp)
-        
-        if col+1<n:
-            self.find(matrix,n,row+1,col+1,self.ans,temp)
-        
-        # temp-=matrix[row][col]
-        
-        
-        return
-        
-        
-        
-    
+       
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         
         n = len(matrix)
         
-        # recursive
-#         self.ans = sys.maxsize
-#         temp = 0
-#         for col in range(n):
-#             self.find(matrix,n,0,col,self.ans,temp)
-#         return self.ans
-        
-        for row in range(1,n):
+        for row in range(1,n):  #start from 2nd row
             for col in range(n):
                 
-                back = sys.maxsize
+                back = sys.maxsize 
                 forward = sys.maxsize
                 
-                if col!=0:
+                if col!=0: #if col is not 0, we can take back col
                     back = matrix[row-1][col-1]
                 
-                if col!=n-1:
+                if col!=n-1: # if col is not ending,we can take next col
                     forward = matrix[row-1][col+1]
                     
-                middle = matrix[row-1][col]
+                middle = matrix[row-1][col] #middle can be always taken
                 
+                # now at each [row,col] calculate what can be min sum with its previous row
+                # as we can only compare 3 col in prev row , it becomes simple
                 
-                # print(row,col)
-                # print(back, " ", middle," ",forward)
                 matrix[row][col]+=min(back,middle,forward)
                 
-        
-        print(matrix)
-        
+        # last row's min is our ans
         return min(matrix[n-1])
                 
                 
