6b0c4254cb7f8a8229f67c0b2e4e59473cfb4887
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-08-23 00:45:23 -0400

    Time: 635 ms (58.33%), Space: 22.1 MB (69.17%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -18,6 +18,24 @@ class Solution:
                           )
             
             return dp[m][n]
+        
+    def solveDP(self,s1,s2,m,n,dp):
+        #intialization
+        for i in range(m+1):
+            for j in range(n+1):
+                if i==0 or j==0:
+                    dp[i][j]=0
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1])
+                    
+        return dp[m][n]
 
     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
         
@@ -26,5 +44,7 @@ class Solution:
         
         dp = [[-1]*(n+1) for _ in range(m+1)]
         
-        return self.getLCS(s1,s2,m,n,dp)
+        # return self.getLCS(s1,s2,m,n,dp)
+        
+        return self.solveDP(s1,s2,m,n,dp)
         
