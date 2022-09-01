de60255fff7a4a5bf2ebd3fe660ef40ed1927138
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-08-31 22:50:03 -0500

    Time: 1909 ms (13.87%), Space: 20.4 MB (27.82%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,31 +1,32 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         
-        l = 0
+        l = 0 #left end of window
         ans = 0
         d = dict()
         
+        # iterate over array
         for r in range(len(fruits)):
             
             r_fruit = fruits[r]
             
+            
             if r_fruit not in d.keys():
-                d[r_fruit]=0
-            d[r_fruit]+=1
+                d[r_fruit]=0  #add if r_fruit not in dict
+            d[r_fruit]+=1 #update count as it is now included
             
-            
+            #if dict len >2 then only we need to check to reduce window as we can only use 2 basket
             while len(d)>2:
                 l_fruit = fruits[l]
                 
-                d[l_fruit]-=1
+                d[l_fruit]-=1 #decrease
                 
-                if d[l_fruit]==0:
+                if d[l_fruit]==0: #if zero no need for that , delete
                     del d[l_fruit]
-                
+                # also move ahead the left end of window
                 l+=1
             
-            ans = max(ans,r-l+1)
-        
+            ans = max(ans,r-l+1) #at each step check and update max        
         
         return ans
                 
