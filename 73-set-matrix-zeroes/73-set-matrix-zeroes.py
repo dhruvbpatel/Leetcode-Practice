abdc6f40124f92a2111b7215df45074ed2d99e0c
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-09-08 22:58:55 -0500

    Time: 265 ms (17.51%), Space: 14.7 MB (54.46%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -4,24 +4,25 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
-        col0 = 1
-        
         n = len(matrix)
         m = len(matrix[0])
+        
+        col0 = 1
+        
         
         for i in range(n):
             
             if matrix[i][0]==0:
-                col0=0
-
+                col0 = 0
+            
             for j in range(1,m):
                 
                 if matrix[i][j]==0:
-                    matrix[i][0]=0
-                    matrix[0][j]=0
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+            
         
         for i in range(n-1,-1,-1):
-            
             for j in range(m-1,0,-1):
                 
                 if matrix[i][0]==0 or matrix[0][j]==0:
@@ -31,7 +32,8 @@ class Solution:
                 matrix[i][0]=0
         
         return matrix
-        
+                
+                
         
                     
                 
