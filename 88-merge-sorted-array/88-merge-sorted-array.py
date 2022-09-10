c48457e18f72f845055b7fe13b858174b9640d54
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-09-09 19:28:10 -0500

    Time: 76 ms (15.81%), Space: 14 MB (38.24%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        idx = m+n-1  ## current pointer idx
+        idx = m+n-1
         p1 = m-1
         p2 = n-1
         
@@ -14,17 +14,18 @@ class Solution:
             if p2<0:
                 break
             
+            
             if p1>=0 and nums1[p1]>nums2[p2]:
-                
                 nums1[idx] = nums1[p1]
                 p1-=1
             else:
                 nums1[idx] = nums2[p2]
                 p2-=1
+                    
+            
         
-        return nums1
-                
-                
-                
-            
-            
+        
+        
+        
+        
+        
