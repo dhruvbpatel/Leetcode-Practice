56801685a6d1fc9ca7f620413f0ec5d29904eb29
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-09-14 18:00:31 -0500

    Time: 1177 ms (98.49%), Space: 25.2 MB (43.26%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,28 +1,56 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
-        #make ans array with zero
-        ans = [0]*len(temperatures)
-        st = [] #maintain stack: monotonic decreasing stack
+        n = len(temperatures)
+        hottest = 0
         
-        for i in range(len(temperatures)):
+        ans = [0]*n
+        
+        for curr in range(n-1,-1,-1):
             
-            curr = temperatures[i] #for eah elements
+            curr_temp = temperatures[curr]
             
-            # if curr is bigger than stack top, do until it is smalller or stack empty
-            while(len(st)!=0 and temperatures[st[-1]]<curr):
+            if curr_temp>=hottest:
+                hottest = curr_temp
+                continue
+            
+            days = 1
+            
+            while temperatures[curr+days]<=curr_temp:
+                days+=ans[curr+days]
                 
-                # get prev idx
-                prev = st[-1]
-                st.pop() #pop
-                diff = i - prev  #get diff 
-                # now as our stack is storing decreasing temperatues, when we get first bigger idx, then 
-                # for that idx diff id the no of days when we get new max temperatures
-                ans[prev] = diff 
+            ans[curr]=days
             
-            # in any other case append idx in stack when elements are smaller
-            st.append(i)
-        
-        
         return ans
+        
+        
+        
+        
+        
+        
+        
+        
+#         #make ans array with zero
+#         ans = [0]*len(temperatures)
+#         st = [] #maintain stack: monotonic decreasing stack
+        
+#         for i in range(len(temperatures)):
+            
+#             curr = temperatures[i] #for eah elements
+            
+#             # if curr is bigger than stack top, do until it is smalller or stack empty
+#             while(len(st)!=0 and temperatures[st[-1]]<curr):
+                
+#                 # get prev idx
+#                 prev = st.pop()#pop
+#                 diff = i - prev  #get diff 
+#                 # now as our stack is storing decreasing temperatues, when we get first bigger idx, then 
+#                 # for that idx diff id the no of days when we get new max temperatures
+#                 ans[prev] = diff 
+            
+#             # in any other case append idx in stack when elements are smaller
+#             st.append(i)
+        
+        
+#         return ans
         
