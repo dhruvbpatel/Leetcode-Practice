8e657581012d683c73365b286bfa68d94ed794d4
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-09-20 14:43:23 -0500

    Time: 116 ms (9.32%), Space: 14.1 MB (77.98%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -3,21 +3,59 @@ class Solution:
         
         #stack approach
         
-        st = [-1]
-        ans =0
+#         st = [-1]
+#         ans =0
         
-        for i in range(len(s)):
+#         for i in range(len(s)):
             
-            if s[i]=='(':
-                st.append(i)
-            else:
+#             if s[i]=='(':
+#                 st.append(i)
+#             else:
                 
-                st.pop()
-                if len(st)==0:
-                    st.append(i)
-                else:
-                    ans = max(ans,i-st[-1])
+#                 st.pop()
+#                 if len(st)==0:
+#                     st.append(i)
+#                 else:
+#                     ans = max(ans,i-st[-1])
+        
+#         return ans
+
+        l = 0
+        r = 0
+        
+        ans = 0
+        
+        #left traverse
+        for i in range(0,len(s)):
+            if s[i]=='(':
+                l+=1
+            else:
+                r+=1
+            
+            if l==r:
+                ans = max(ans,2*r)
+            
+            elif r>=l:
+                l,r=0,0
+        
+        #right traverse
+        l,r=0,0
+        
+        for i in range(len(s)-1,-1,-1):
+            if s[i]=='(':
+                l+=1
+            else:
+                r+=1
+            
+            if l==r:
+                ans = max(ans,2*l)
+            
+            elif l>r:
+                l,r=0,0
         
         return ans
         
+                
+                
+            
         
