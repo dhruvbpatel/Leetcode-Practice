041737c5e9e1c2b974d0e11c72d0ce4d12d70ae0
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-11-09 14:57:16 -0600

    Time: 124 ms (74.44%), Space: 14 MB (66.67%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,42 +1,32 @@
 class Solution:
     
-    def fill(self,image,r,c,color,curr_color):
         
-        
-        if r<0 or c<0 or r>=len(image) or c>=len(image[0]) or image[r][c]!=curr_color:
+    def helper(self,image,r,c,color,m,n,start):
+
+        if r<0 or r>=m or c<0 or c>=n or image[r][c]!=start:
             return
+            
         
-        if image[r][c]==color:
-            return
+        if image[r][c]==start:
+            image[r][c]=color
+
+        self.helper(image,r+1,c,color,m,n,start)
+        self.helper(image,r-1,c,color,m,n,start)
+        self.helper(image,r,c-1,color,m,n,start)
+        self.helper(image,r,c+1,color,m,n,start)
         
-        
-        image[r][c]=color
-        
-        
-        
-        self.fill(image,r,c-1,color,curr_color)
-        self.fill(image,r+1,c,color,curr_color)
-        self.fill(image,r,c+1,color,curr_color)
-        self.fill(image,r-1,c,color,curr_color)
-        
-        
-    
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        m = len(image)
-        n = len(image[0])
-        
-        if image[sr][sc]==color:
+        m=len(image)
+        n=len(image[0])
+
+        start = image[sr][sc]
+
+        if start==color:
             return image
-        
-        
-        curr_color = image[sr][sc]
-        
-        self.fill(image,sr,sc,color,curr_color)
-        
+
+        self.helper(image,sr,sc,color,m,n,start)
+
         return image
-        
-        
-        
         
         
