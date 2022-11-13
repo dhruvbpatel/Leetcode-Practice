13e50b10540e04ec923ffe8d7d54617180fbd9ea
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-11-12 19:43:59 -0600

    Time: 32 ms (92.13%), Space: 13.8 MB (98.37%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,16 +1,28 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         
+        # Explanation
+        # Basic idea:
+        # Find a index i, where there is a increasing order
+        # On the right side of i, find the max value (max_val) and its index (max_idx)
+        # On the left side of i, find the most left value and its index (left_idx), which is less than max_val
+        # Swap above left_idx and max_idx if necessary
+        # Please check the comments for more detail
+        
         s = list(str(num))
         
         n = len(s)
         
+        flag = False
+        
         for i in range(n-1):
             
             if s[i]<s[i+1]:
+                flag = True
                 break
         
-        else: return num
+        if flag==False:
+            return num
         
         mx_idx,mx_val = i+1,s[i+1]
         
