214eea3c57660ec2ec4b7318a581ef57d9158102
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-11-12 18:43:25 -0600

    Time: 912 ms (17.47%), Space: 17.5 MB (48.10%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -0,0 +1,47 @@
+class Solution:
+    def wallsAndGates(self, mat: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        
+        m = len(mat)
+        n = len(mat[0])
+        
+        visited = [[0]*n for _ in range(m)]
+        
+        # ans = [[0]*n for _ in range(m)]
+        
+        q = []
+        
+        
+        
+        #add all 0 in queue
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c]==0:
+                    q.append([r,c,0]) #it itself is at 0 distance
+                    visited[r][c]=1 # also mark visited
+        
+        
+        drow = [-1,0,1,0]
+        dcol = [0,1,0,-1]
+        
+        while q:
+            
+            curr = q.pop(0)
+            r = curr[0]
+            c = curr[1]
+            steps = curr[2]
+            
+            mat[r][c]=steps  #add in ans array
+            
+            for i in range(4):
+                #for all 4 directions at that step
+                nrow = r+drow[i]
+                ncol = c+dcol[i]
+                # if r,c valid and not visited add in q with step+1
+                if nrow>=0 and nrow<m and ncol>=0 and ncol<n and visited[nrow][ncol]==0 and mat[nrow][ncol]!=-1:
+                    visited[nrow][ncol]=1
+                    q.append([nrow,ncol,steps+1])
+            
+        # return ans
