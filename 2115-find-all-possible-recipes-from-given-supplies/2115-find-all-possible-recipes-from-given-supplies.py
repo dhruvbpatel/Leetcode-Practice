15c6931fc6244ceedd5d111f2fc15f4e80c22d5e
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-11-12 23:02:06 -0600

    Time: 2396 ms (41.36%), Space: 17.1 MB (67.89%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,30 +1,30 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         
-        graph = defaultdict(list)
-        in_degree = defaultdict(int)
+        graph = defaultdict(list) #graph
+        in_degree = defaultdict(int) #in degree, used for topological ordering
         
         for r,ing in zip(recipes,ingredients):
             for i in ing:
-                graph[i].append(r)
-                in_degree[r]+=1
+                graph[i].append(r) #build graph
+                in_degree[r]+=1 #add indegree for recipes
         
         
-        q = supplies[:]
+        q = supplies[:] #add all supplies in q
         ans = []
         
         while q:
             
-            ing = q.pop(0)
+            ing = q.pop(0) #pop 1st element
             
-            if ing in recipes:
-                ans.append(ing)
+            if ing in recipes:  #if we get ing in recipes ,it means it was added after it's all dependencies were travelled, i.e pre-req were met
+                ans.append(ing) # so simply add
             
-            for child in graph[ing]:
+            for child in graph[ing]: #iterate in childs of curr ingredients and decrease indg of child
                 in_degree[child]-=1
             
-                if in_degree[child]==0:
-                    q.append(child)
+                if in_degree[child]==0: #if 0 i.e met all requirements
+                    q.append(child) #append 
         
         return ans
             
