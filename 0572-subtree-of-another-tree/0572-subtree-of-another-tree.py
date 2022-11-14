057583dc0f5a8c554c37f196e628e9bea514efa6
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-11-13 18:42:55 -0600

    Time: 260 ms (55.80%), Space: 15.1 MB (64.89%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -8,23 +8,23 @@ class Solution:
     
     def isSame(self,root,s):
         
-        if root is None or s is None:
-            return root==s
+        if not root or not s:  
+            return root==s # return true if both are same else false
         
+        # condition to check same
         return root.val==s.val and self.isSame(root.left,s.left) and self.isSame(root.right,s.right)
     
     
     def isSubtree(self, root: Optional[TreeNode], s: Optional[TreeNode]) -> bool:
         
-        if not root:
+        if not root:  # if no root return false
             return False
-        
-        if not s:
+
+        if not s: # if s is null, means it still exist in main tree
             return True
         
-        if self.isSame(root,s):
+        if self.isSame(root,s): #check same
             return True
         
-        return self.isSubtree(root.left,s) or self.isSubtree(root.right,s)
-    
+        return self.isSubtree(root.left,s) or self.isSubtree(root.right,s) # if not found check on left and right
         
