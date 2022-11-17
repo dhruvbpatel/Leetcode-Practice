4a950f736ae9aa3988e85fac64f0ee1f89a6a944
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2022-11-16 22:58:23 -0600

    Time: 807 ms (83.33%), Space: 18.1 MB (19.35%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -12,12 +12,12 @@ class Solution:
         
         
         
-        #add all 1 in queue
+        #add all 0 in queue
         for r in range(m):
             for c in range(n):
                 if mat[r][c]==0:
-                    q.append([r,c,0])
-                    visited[r][c]=1
+                    q.append([r,c,0]) #it itself is at 0 distance
+                    visited[r][c]=1 # also mark visited
         
         
         drow = [-1,0,1,0]
@@ -30,17 +30,18 @@ class Solution:
             c = curr[1]
             steps = curr[2]
             
-            ans[r][c]=steps
+            ans[r][c]=steps  #add in ans array
             
             for i in range(4):
-                
+                #for all 4 directions at that step
                 nrow = r+drow[i]
                 ncol = c+dcol[i]
-                
+                # if r,c valid and not visited add in q with step+1
                 if nrow>=0 and nrow<m and ncol>=0 and ncol<n and visited[nrow][ncol]==0:
                     visited[nrow][ncol]=1
                     q.append([nrow,ncol,steps+1])
                     
         return ans
         
+        #TC: MxNx4 == O(MN)
         
