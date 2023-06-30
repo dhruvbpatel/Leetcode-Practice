3c4fc9b9a08f71494a1cf048d0a10a4186c55b95
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2023-06-30 17:27:43 -0500

    Time: 80 ms (56.56%), Space: 17.7 MB (16.48%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        mp = defaultdict(int)
+        ans = []
+        
+        for i in range(len(nums)):
+            
+            find = target-nums[i]
+            
+            if find in mp.keys():
+                ans.append(i)
+                ans.append(mp[find])
+                return ans
+            
+            mp[nums[i]]=i
+        
+        
