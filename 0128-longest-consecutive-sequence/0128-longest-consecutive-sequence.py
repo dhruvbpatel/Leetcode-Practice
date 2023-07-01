395aea2ca316badb7c1bb1a281b676421617bb20
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2023-06-30 19:29:00 -0500

    Time: 1974 ms (42.88%), Space: 32.2 MB (15.23%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        logic
+        
+          // every time check if number smaller than curr is in mp or not
+        // if there then skip
+        // if not then we have arrived at smallest number in possible sequence
+        // then untill num++ is not in mp , increase val and check if in mp
+        // also do count ,and max cout will be ans
+                
+        """
+        
+         
+        st = set(nums)
+        ans = 0
+        
+        
+        for n in nums:
+            
+            if n-1 in st: #not smallest number so continue
+                continue
+            else:
+                
+                count = 1
+                
+                while(n+count in st):
+                    count+=1
+                    
+                ans = max(count,ans)
+        return ans
+
+                
+                
