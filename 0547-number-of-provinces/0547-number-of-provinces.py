adac5251464e69f354482364aabb86eaf057de01
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2023-07-09 18:33:40 -0500

    Time: 230 ms (27.06%), Space: 17.3 MB (18.85%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,35 +1,25 @@
 class Solution:
     
-#     def dfs(self,isConnected,visited,curr):
+    def dfs(self,visited,isConnected,curr):
         
-#         for j in range(len(isConnected)):
-            
-#             if isConnected[curr][j]==1 and visited[j]==0:
-#                 visited[j]=1
-#                 self.dfs(isConnected,visited,j)
-#         return
-        
-    
-    def dfs(self,isConnected,visited,curr):
+        visited[curr]=1
         
         for j in range(len(isConnected)):
             
             if isConnected[curr][j]==1 and visited[j]==0:
-                visited[j]=1
-                self.dfs(isConnected,visited,j)
-        return
-    
+                self.dfs(visited,isConnected,j)
+        
+        
     
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited=[0]*n
+        ans=0
         
-        visited = [0]*len(isConnected)
-        ans = 0
-        
-        for i in range(len(isConnected)):
-            
+        for i in range(n):
             if visited[i]==0:
-                self.dfs(isConnected,visited,i)
+                self.dfs(visited,isConnected,i)
                 ans+=1
-                
+        
         return ans
         
